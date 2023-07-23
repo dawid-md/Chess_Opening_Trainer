f555d8b8c128709d41c1d9f1c6f2de54dc94b58d
Dawid Mądrzyk <maddawid@gmail.com>
2023-07-23 23:58:18 +0200

    formatting function for pgn creator

--- src/tests.js
+++ src/tests.js
@@ -1,18 +1,41 @@
+class treeNode {
+  constructor(result, parent = null) {
+      if (result == 'root') {
+          this.fen = 'rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1';
+      } else {
+          this.move = result.san;
+          this.fen = result.after;
+          this.parent = parent;
+      }
+      this.children = [];
+  }
+
+  addChild(node) {
+      node.parent = this;
+      this.children.push(node);
+  }
+}
+
 let pgnTree = {
     move: "startingPosition",
     children: [
       {
-        move: "e4",
+        move: "Nf3",
         children: [
           {
-            move: "e5",
+            move: "d5",
             children: [
               {
-                move: "nf3",
-                children: []
+                move: "d4",
+                children: [              
+                  {
+                    move: "Nf6",
+                    children: []
+                  }
+                ]
               },
               {
-                move: "d4",
+                move: "c4",
                 children: []
               }
             ]
@@ -22,98 +45,49 @@ let pgnTree = {
     ]
 }
 
+function treeToPGN(node, depth = -1) {
+  let pgn = '';
+  let isFollowingVariation = false;
 
+  if (depth % 2 === 0) {
+      const moveNumber = Math.floor(depth / 2) + 1;
+      pgn += `${moveNumber}. `;
+  } else if (node.parent && node.parent.children.length > 1 && node === node.parent.children[1]) {
+      // Check if the current node is a direct child of a node with more than one child
+      // (indicating it's following a variation).
+      isFollowingVariation = true;
+      const moveNumber = Math.floor((depth + 1) / 2);
+      pgn += `${moveNumber}... `;
+  }
 
+  if (node.move) {
+      pgn += node.move + ' ';
+  }
 
+  if (node.children.length == 1) {
+      let mainLine = treeToPGN(node.children[0], depth + 1);
+      pgn += mainLine;
 
+      if (node.children.length > 1) {
+          let variations = node.children.slice(1).map(child => `(${treeToPGN(child, depth + 1)})`).join(' ');
+          pgn += ' ' + variations;
+      }
+  }
 
+  return pgn.trim();
+}
 
+let root = new treeNode('root');
+let nf3 = new treeNode({ san: 'Nf3' }, root);
+let d5 = new treeNode({ san: 'd5' }, nf3);
+let d4 = new treeNode({ san: 'd4' }, d5);
+let nf6 = new treeNode({ san: 'Nf6' }, d4);
+let c4 = new treeNode({ san: 'c4' }, d5);
 
-// let ax = ["e4", "e5",                                       //e5 is main line
-//                 ["nf6", "nf3", "d6", ["c6", "d3"]],         //first variant - instead of e5
-//                 ["c5", "nf3"],                              //second variant - instead of e5
-//                     "d3"]                                   //continuation of main line - e4, e5, d3
+root.addChild(nf3);
+nf3.addChild(d5);
+d5.addChild(d4);
+d5.addChild(c4);
+d4.addChild(nf6);
 
-
-//teraz wykonujac ruch trzeba na podstawie jego current index sprawdzic czy istnieje juz ruch ktory ma ten sam current index i ten sam fen
-//jesli tak - to nie tworzymy nowego ruchu w tablicy moves tylko zwiększamy currentindex lub 
-//jesli ten ruch jest istniejacym variantem to dodajemy nowy element do currentIndex (1) i to on jest naszym currentIndexem teraz.
-    //oznacza to, że nasz currentIndex wygląda tak: [3,1]   -- trzeba sie zastanowic czy variant we variancie powinien wygenerowac nowy index czy nowy array z indexem
-//jesli main line juz istnieje i jest rozny od aktualnego ruchu to tworzymy nowy variant
-
-//console.log(ax);
-
-// if((moves.length > currentMoveIndex[0] && variation.length == 0) || variation.length > 0){
-//     let foundMatch = false
-//     let j = currentMoveIndex[currentMoveIndex.length-1]
-
-//     console.log(result);
-
-//     // If the first item is a string, compare it with result.san
-//     //if (typeof moves[j] === 'string') {
-//       foundMatch = moves[j].after === result.after;
-//       console.log(foundMatch);
-//       j++;
-//     //}
-
-//     // If first comparison was not a match, continue checking the next items (they are arrays)
-//     while(!foundMatch && j < moves.length && Array.isArray(moves[j])) {
-//       if(moves[j].length > 0 && moves[j][0] === result.san) {
-//           foundMatch = true;
-//       }
-//       j++;
-//     }
-
-//     if (!foundMatch && variation.length == 0) {
-//       setVariation([...variation, result])  //this line only executes if no match is found
-//       setcurrentMoveIndex(prev => [...prev, 1])
-//     }
-//     else if (!foundMatch && currentMoveIndex[currentMoveIndex.length - 1] == variation.length) {
-//       setVariation([...variation, result])
-//       setcurrentMoveIndex(prev => {
-//         let newState = [...prev];
-//         newState[newState.length - 1] = newState[newState.length - 1] + 1
-//         return newState;
-//       })
-//     }
-//     else{
-//       console.log("length of moves " + moves.length);
-//     }
-//   }
-
-//   else if (currentMoveIndex.length > 1) {
-//     if(currentMoveIndex[currentMoveIndex.length - 1] < variation.length){
-//       setVariation(prev => [...prev, [result]])
-//     }
-//   }
-
-//   else{
-//     setMoves([...moves, result])
-//     setcurrentMoveIndex(prev => {
-//       let newState = [...prev];
-//       newState[newState.length - 1] = newState[newState.length - 1] + 1
-//       return newState;
-//     })
-//   } 
-
-// //moveback
-
-// if(variation.length > 0 && currentMoveIndex[currentMoveIndex.length -1] == 1){  //it means zero before state is updated from 1 to 0
-//     const newMoves = moves
-//     newMoves.splice(currentMoveIndex[0]+1, 0, variation)
-//     setMoves(newMoves)
-//     setVariation([])
-//     setcurrentMoveIndex(prev => {
-//       let newState = [...prev]
-//       newState.splice(-1,1)
-//       return newState
-//     })
-//   }
-
-//   else {
-//     setcurrentMoveIndex(prev => {
-//       let newState = [...prev]
-//       newState[newState.length - 1] = newState[newState.length - 1] - 1
-//       return newState
-//     })
-//   }
+console.log(treeToPGN(root));
