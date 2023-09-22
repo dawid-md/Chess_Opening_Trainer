c1d31403fa216aa8baca8cb1e0a8a02194b7bb5c
Dawid Mądrzyk <maddawid@gmail.com>
2023-09-23 01:40:31 +0200

    adding rectangle to the top of the square

--- src/Multiplayer.js
+++ src/Multiplayer.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, forwardRef } from "react";
 import { Chess } from "chess.js";
 import { Chessboard } from "react-chessboard";
 import useSound from "use-sound"
@@ -28,7 +28,7 @@ export default function Multiplayer() {
 
     setGame(new Chess(newPosition.fen()))
     return result;
-}
+  }
 
   function onDrop(sourceSquare, targetSquare) {
     const move = {
@@ -43,14 +43,44 @@ export default function Multiplayer() {
     return true;
   }
 
+  const CustomSquareRenderer = forwardRef((props, ref) => {
+    const { children, square, style } = props;
+
+    const squareStyle = {
+      ...style,
+      position: 'relative'  // Ensures child elements with absolute positioning are relative to this square
+    };
+
+    return (
+      <div ref={ref} style={squareStyle}>
+        {children}
+        {square === "a1" && (
+          <div 
+            style={{
+              position: "absolute",
+              bottom: 0,
+              right: 0,
+              width: '20px',  // Adjust size as needed
+              height: '20px',
+              //borderRadius: '50%',  // Makes it a circle
+              backgroundColor: 'brown'
+            }} 
+          />
+        )}
+      </div>
+    );
+  });
+
+
   return(
-  <div className="mainDiv">
-    <div className="chessboardDiv w-75">
-      <Chessboard 
-        position={game.fen()} 
-        onPieceDrop={onDrop} 
-      />
+    <div className="mainDiv">
+      <div className="chessboardDiv w-75">
+        <Chessboard 
+          position={game.fen()} 
+          customSquare={(props) => <CustomSquareRenderer {...props} />}
+          onPieceDrop={onDrop} 
+        />
+      </div>
     </div>
-  </div>
   )
 }
